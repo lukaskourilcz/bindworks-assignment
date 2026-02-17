97450737af3214fc0ed577ec6969b6a83c121646
Lukas Kouril <kouril.lukas@gmail.com>
2026-02-17 20:31:54 +0100

    feat: dockerfiles

--- .next/dev/server/app/api/todos/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/todos/route.js")
-R.c("server/chunks/41ad5_next_da76fa39._.js")
-R.c("server/chunks/[root-of-the-server]__7f156be7._.js")
-R.c("server/chunks/_next-internal_server_app_api_todos_route_actions_7f61f60d.js")
-R.m("[project]/node_modules/.pnpm/next@16.1.1_react-dom@19.2.3_react@19.2.3__react@19.2.3/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/todos/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/.pnpm/next@16.1.1_react-dom@19.2.3_react@19.2.3__react@19.2.3/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/todos/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HEE5Ik3tISBkhfUhjfhEszVd33EdV9wUjF3tGaw8lvk=\"\n}"
